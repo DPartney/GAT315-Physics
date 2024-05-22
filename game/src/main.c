cbfb62fbb64d5aa5a624985f22f9ebce35465d56
DPartney <dpartney@student.neumont.edu>
2024-05-21 20:22:07 -0600

    Code cleanup

--- game/src/main.c
+++ game/src/main.c
@@ -14,88 +14,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-//#define MAX_BODIES 10000
-
-//void FireworkOne() 
-//{
-//	Vector2 position = GetMousePosition();
-//	for (int i = 0; i < 500; i++) 
-//	{
-//		ncBody* body = CreateBody();
-//		body->position = position;
-//		body->mass = GetRandomFloatValue(1, 5);
-//		body->inverseMass = 1 / body->mass;
-//		body->type = BT_DYNAMIC;
-//		body->damping = 0.3f;
-//		body->gravityScale = 3;
-//		body->color = (Color){ GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), 255 };
-//
-//		float angle = GetRandomFloatValue(0, 2 * PI);
-//
-//		float forceX = cos(angle) * GetRandomFloatValue(0, 200);
-//		float forceY = sin(angle) * GetRandomFloatValue(0, 200);
-//
-//		ApplyForce(body, (Vector2) { forceX, forceY }, FM_VELOCITY);
-//	}
-//}
-//
-//void FireworkTwo()
-//{
-//	Vector2 position = GetMousePosition();
-//	for (int i = 0; i < 500; i++)
-//	{
-//		ncBody* body = CreateBody();
-//		body->position = position;
-//		body->mass = GetRandomFloatValue(1, 5);
-//		body->inverseMass = 1 / body->mass;
-//		body->type = BT_DYNAMIC;
-//		body->damping = 0;
-//		body->gravityScale = 1;
-//		body->color = (Color){ GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), 255 };
-//
-//		float angle = GetRandomFloatValue(PI, 2 * PI);
-//
-//		float forceX = cos(angle) * GetRandomFloatValue(0, 50);
-//		float forceY = sin(angle) * GetRandomFloatValue(0, 500);
-//
-//		ApplyForce(body, (Vector2) { forceX, forceY }, FM_VELOCITY);
-//	}
-//}
-//
-//void FireworkThree() 
-//{
-//	Vector2 position = GetMousePosition();
-//	float endAngle = 10 * PI;
-//	float startAngle = 0;
-//	float bodyCount = 500;
-//
-//	float angleIncrement = (endAngle - startAngle) / bodyCount;
-//	float radius = 0;
-//	float height = 0;
-//
-//	for (int i = 0; i < bodyCount; i++)
-//	{
-//		// Calculate position in spiral
-//		Vector2 spiralPosition = (Vector2){ radius * cos(startAngle + i * angleIncrement) , radius * sin(startAngle + i * angleIncrement) };
-//
-//		// Set body position relative to center
-//		ncBody* body = CreateBody();
-//		body->position = position;
-//		body->mass = GetRandomFloatValue(1, 5);
-//		body->inverseMass = 1 / body->mass;
-//		body->type = BT_DYNAMIC;
-//		body->damping = 0.1;
-//		body->gravityScale = 5;
-//		body->color = (Color){ GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), GetRandomFloatValue(0,255), 255 };
-//			
-//		ApplyForce(body, spiralPosition, FM_VELOCITY);
-//
-//		// Increment radius and height for next body
-//		radius += 0.5f;
-//		height += 0.5f;
-//	}
-//}
-
 int main(void)
 {
 	ncBody* selectedBody = NULL;
@@ -107,8 +25,6 @@ int main(void)
 
 	float fixedTimestep = 1.0f / 60;
 	float timeAccumulator = 0.0f;
-
-	//initialize world
 
 	//game loop
 	while (!WindowShouldClose())
@@ -186,8 +102,6 @@ int main(void)
 		//stats
 		DrawText(TextFormat("FPS: %.2f (%.2fms)", fps, 1000 / fps), 10, 10, 20, LIME);
 		DrawText(TextFormat("Frame: %.4f", dt), 10, 30, 20, LIME);
-
-		//DrawCircle((int)position.x, (int)position.y, 20, PINK);
 
 		// draw bodies
 		for (ncBody* body = ncBodies; body; body = body->next)
